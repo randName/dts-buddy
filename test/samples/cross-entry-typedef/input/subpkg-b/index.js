99a582de54475157ba0963dd182be7768b8100ad
Shun Git <kwokshungit@gmail.com>
2026-07-18 15:44:56 +0800

    its always windows

--- test/samples/cross-entry-typedef/input/subpkg-b/index.js
+++ test/samples/cross-entry-typedef/input/subpkg-b/index.js
@@ -1,6 +1,6 @@
 export { makeFoo } from '../subpkg-a/index.js';
 
-/** @typedef {import('../subpkg-a').Foo} Foo */
+/** @typedef {import('../subpkg-a/index.js').Foo} Foo */
 
 /** @param {Foo} f */
 export function takeFoo(f) {
